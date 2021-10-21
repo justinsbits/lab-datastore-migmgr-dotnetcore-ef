b4b3486399c5c76e0a384626fb399d0ccedefe1c
Justin <justinsbits@gmail.com>
2021-10-21 06:36:11 -0700

    initial commit

---
+++ Migrations/20211021001458_init.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommanderDBMigrationMgr.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tools",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tools", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HowTo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommandLine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ToolId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commands_Tools_ToolId",
+                        column: x => x.ToolId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tools",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "", "dotnet" });
+
+            migrationBuilder.InsertData(
+                table: "Tools",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "", "docker" });
+
+            migrationBuilder.InsertData(
+                table: "Commands",
+                columns: new[] { "Id", "CommandLine", "HowTo", "ToolId" },
+                values: new object[] { 1, "dotnet user-secrets init", "Enable secret storage for project", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Commands",
+                columns: new[] { "Id", "CommandLine", "HowTo", "ToolId" },
+                values: new object[] { 2, "dotnet user-secrets set \"<key>\" \"<value>\"", "Set secret for project", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commands_ToolId",
+                table: "Commands",
+                column: "ToolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tools_Name",
+                table: "Tools",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+
+            migrationBuilder.DropTable(
+                name: "Tools");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using CommanderDA;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CommanderDBMigrationMgr
+{
+    class Program
+    {
+        // https://docs.microsoft.com/en-us/dotnet/core/extensions/generic-host
+        public static void Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<AppDbContext>();
+                dbContext.Database.Migrate();
+            }
+
+            host.Run();
+        }
+
+        // EF Core uses this method at design time to access the DbContext
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((hostContext, builder) =>
+                {
+                    if (hostContext.HostingEnvironment.IsDevelopment())
+                    {
+                        builder.AddUserSecrets<Program>();
+                    }
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var sqlConStrBuilder = new SqlConnectionStringBuilder(
+                    hostContext.Configuration.GetConnectionString("CommandConStr"));
+                    sqlConStrBuilder.Password = hostContext.Configuration["DbPassword"];
+                    string sqlConStr = sqlConStrBuilder.ConnectionString;
+
+                    services
+                        .AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConStr, b => b.MigrationsAssembly("CommanderDBMigrationMgr")));
+                });
+        }
+
+        // Example of working with custom config
+        //
+        //public static IHostBuilder CreateHostBuilder(string[] args)
+        //{
+        //    IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+        //    String sqlServerConStr = null;
+        //    hostBuilder = hostBuilder.ConfigureHostConfiguration(configHost =>
+        //        {
+        //            IConfiguration config = configHost.SetBasePath(Directory.GetCurrentDirectory())
+        //               .AddJsonFile("commanderDBSettings.json", optional: false, reloadOnChange: true)
+        //               .AddEnvironmentVariables()
+        //               .AddCommandLine(args).Build();
+        //            sqlServerConStr = config.GetValue<string>("ConnectionStrings:CommandConStr");
+        //        });
+
+        //    hostBuilder.ConfigureServices((hostContext, services) =>
+        //    {
+        //        services
+        //            .AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlServerConStr, b => b.MigrationsAssembly("CommanderDBMigrationMgr")));
+        //    });
+
+        //    return hostBuilder;
+        //}
+
+
+        // Example using direct DbContext creation (but ef tools then not usable since it relies on finding and applying ".Build" against a method that addresses "CreateHostBuilder")
+        //
+        //static void Main(string[] args)
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //       .SetBasePath(Directory.GetCurrentDirectory())
+        //       .AddJsonFile("commanderDBSettings.json", optional: false, reloadOnChange: true)
+        //       .AddEnvironmentVariables()
+        //       .AddCommandLine(args);
+        //    IConfiguration config = builder.Build();
+        //    var sqlServerConStr = config.GetValue<string>("ConnectionStrings:CommandConStr");
+        //    var options = new DbContextOptionsBuilder<AppDbContext>().UseSqlServer(sqlServerConStr).Options;
+        //    using (var ctx2 = new AppDbContext(options))
+        //    {
+        //        ctx2.Database.Migrate();
+        //    }
+        //}
+
+
+        // Playing with ServiceCollection / Provider here (but then you lost benefit of the hosted object that is a one-stop-shop for obj liftime mgmt
+        // 
+        //static readonly ServiceCollection Services = new ServiceCollection();
+        //static void Main(string[] args)
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //       .SetBasePath(Directory.GetCurrentDirectory())
+        //       .AddJsonFile("commanderDBSettings.json", optional: false, reloadOnChange: true)
+        //       .AddEnvironmentVariables()
+        //       .AddCommandLine(args);
+        //    IConfiguration config = builder.Build();
+
+        //    var sqlServerConStr = config.GetValue<string>("ConnectionStrings:CommandConStr");
+        //    Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlServerConStr)
+        //     .LogTo(
+        //         Console.WriteLine, // ...or log to file etc.
+        //         new[] { DbLoggerCategory.Database.Command.Name,
+        //                            DbLoggerCategory.Database.Transaction.Name },
+        //         LogLevel.Debug));
+        //    ServiceProvider serviceProvider = Services.BuildServiceProvider();
+        //    var ctx = serviceProvider.GetService<AppDbContext>();
+        //    ctx.Database.EnsureCreated();
+        //    ctx.Database.Migrate();
+
+        //}
+    }
+
+}
